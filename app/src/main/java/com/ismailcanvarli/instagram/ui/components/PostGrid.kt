18e4a62051a77509719447cf2daf5a4a1dc405f5
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-21 00:18:46 +0300

    Add real photos to the post grid section

--- app/src/main/java/com/ismailcanvarli/instagram/ui/components/PostGrid.kt
+++ app/src/main/java/com/ismailcanvarli/instagram/ui/components/PostGrid.kt
@@ -4,14 +4,12 @@ package com.ismailcanvarli.instagram.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -37,13 +35,7 @@ fun PostsGrid() {
         R.drawable.photo9,
         R.drawable.photo10,
         R.drawable.photo11,
-        R.drawable.photo12,
-        R.drawable.photo13,
-        R.drawable.photo14,
-        R.drawable.photo15,
-        R.drawable.photo16,
-        R.drawable.photo17,
-        R.drawable.photo18
+        R.drawable.photo12
     )
     Column(
         modifier = Modifier
@@ -52,8 +44,7 @@ fun PostsGrid() {
     ) {
         for (row in photos.chunked(3)) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(2.dp)
+                modifier = Modifier.fillMaxWidth()
             ) {
                 for (photo in row) {
                     Image(
@@ -62,7 +53,6 @@ fun PostsGrid() {
                         modifier = Modifier
                             .weight(1f)
                             .aspectRatio(1f)
-                            .padding(1.dp)
                             .background(Color.Gray, shape = RoundedCornerShape(4.dp)),
                         contentScale = ContentScale.Crop
                     )
