628d63295c320cddb79510a6129aa4cb6e1a56ce
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-21 18:01:51 +0300

    Change font weight to bold

--- app/src/main/java/com/ismailcanvarli/instagram/ui/components/ProfileStat.kt
+++ app/src/main/java/com/ismailcanvarli/instagram/ui/components/ProfileStat.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,10 +21,17 @@ fun ProfileStat(label: String, description: String) {
         modifier = Modifier.padding(horizontal = 8.dp)
     ) {
         Text(
-            text = label, color = Color.Black, fontSize = 18.sp, textAlign = TextAlign.Center
+            text = label,
+            color = Color.Black,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = description, color = Color.Black, fontSize = 14.sp, textAlign = TextAlign.Center
+            text = description,
+            color = Color.Black,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
